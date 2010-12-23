a2be3f7ce36b885ed9dc7cec04ad9118df68d7bb
Joel <joelbm24@gmail.com>
2010-12-23 14:29:37 -0800

    first commit

---
+++ lib/puma.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+import yaml, boto, os
+from boto.ec2.regioninfo import RegionInfo
+
+class Puma():
+    def __init__(self):
+        self.puma_dir = os.environ["HOME"] + "/.puma"
+        try:
+            self.config = yaml.load(file(self.puma_dir + "/config.yml", "r"))
+        except:
+            print "ERROR: No config file. need to make one"
+
+    def connect(self):
+        return boto.connect_ec2(aws_access_key_id=self.config["access_id"],
+                                aws_secret_access_key=self.config["access_secret"],
+                                is_secure=False,
+                                region=RegionInfo(None, self.config.get('location', "nova"),
+                                                  self.config.get('ip', "10.255.24.10")),
+                                port=self.config.get('port', 8773),
+                                path=self.config.get('path', "services/Cloud"))
+    def get_image_list(self):
+        return [image.id for image in self.connect().get_all_images()]
+
+    def get_instance_list(self):
+        return sum([instance.instances for instance in self.connect().get_all_instances()], [])
+
+    def run_instance(self, image_name):
+        image = self.get_image_list()
+        try:
+            print self.get_image_list()[image.index(image_name)]
+        except:
+            print "Image does not exist"
+
